8e1989c71e6b14ff330bbc38709b89c3edd0b338
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2020-04-24 16:49:13 +0200

    Outline main tasks

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const execa = require('execa');
 const Listr = require('Listr');
 
 const tasks = new Listr([
   {
-    title: 'Hello world',
-    task: () => execa('say', ['Hello world']),
+    title: 'Create directory structure',
+    task: () => {},
+  },
+  {
+    title: 'Create PostCSS configuration',
+    task: () => {},
+  },
+  {
+    title: 'Create templates',
+    task: () => {},
+  },
+  {
+    title: 'Install dependencies',
+    task: () => {},
   },
 ]);
 
